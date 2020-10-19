dfa0cc740cc2c39ab880268a965ab095e4162b65
jlkiri <interalia.dev@gmail.com>
2020-10-19 23:20:35 +0900

    Local variables

--- compiler.c
+++ compiler.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "common.h"
 #include "compiler.h"
@@ -41,7 +42,22 @@ typedef struct
   Precedence precedence;
 } ParseRule;
 
+typedef struct
+{
+  Token name;
+  int depth;
+} Local;
+
+typedef struct
+{
+  Local locals[UINT8_COUNT];
+  int local_count;
+  int scope_depth;
+} Compiler;
+
 Parser parser;
+
+Compiler *current = NULL;
 
 Chunk *compiling_chunk;
 
@@ -157,6 +173,13 @@ static void emit_constant(Value value)
   emit_bytes(OP_CONSTANT, make_constant(value));
 }
 
+static void init_compiler(Compiler *compiler)
+{
+  compiler->local_count = 0;
+  compiler->scope_depth = 0;
+  current = compiler;
+}
+
 static void end_compiler()
 {
   emit_return();
@@ -166,6 +189,22 @@ static void end_compiler()
     disassemble_chunk(current_chunk(), "code");
   }
 #endif
+}
+
+static void begin_scope()
+{
+  current->scope_depth++;
+}
+
+static void end_scope()
+{
+  current->scope_depth--;
+
+  while (current->local_count > 0 && current->locals[current->local_count - 1].depth > current->scope_depth)
+  {
+    emit_byte(OP_POP);
+    current->local_count--;
+  }
 }
 
 static void expression();
@@ -179,14 +218,67 @@ static uint8_t identifier_constant(Token *name)
   return make_constant(OBJ_VAL(copy_string(name->start, name->length)));
 }
 
+static bool identifiers_equal(Token *a, Token *b)
+{
+  if (a->length != b->length)
+    return false;
+  return memcmp(a->start, b->start, a->length) == 0;
+}
+
+static void add_local(Token name)
+{
+  if (current->local_count == UINT8_COUNT)
+  {
+    error("Too many local variables in function.");
+    return;
+  }
+
+  Local *local = &current->locals[current->local_count++];
+  local->name = name;
+  local->depth = current->scope_depth;
+}
+
+static void declare_variable()
+{
+  if (current->scope_depth == 0)
+    return;
+
+  Token *name = &parser.previous;
+  for (int i = current->local_count - 1; i >= 0; i--)
+  {
+    Local *local = &current->locals[i];
+    if (local->depth != -1 && local->depth < current->scope_depth)
+    {
+      break;
+    }
+
+    if (identifiers_equal(name, &local->name))
+    {
+      error("Already variable with this name in this scope.");
+    }
+  }
+
+  add_local(*name);
+}
+
 static uint8_t parse_variable(const char *error_message)
 {
   consume(TOKEN_IDENTIFIER, error_message);
+
+  declare_variable();
+  if (current->scope_depth > 0)
+    return 0;
+
   return identifier_constant(&parser.previous);
 }
 
 static void define_variable(uint8_t global)
 {
+  if (current->scope_depth > 0)
+  {
+    return;
+  }
+
   emit_bytes(OP_DEFINE_GLOBAL, global);
 }
 
@@ -391,6 +483,16 @@ static void expression()
   parse_precedence(PREC_ASSIGNMENT);
 }
 
+static void block()
+{
+  while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF))
+  {
+    declaration();
+  }
+
+  consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
 static void var_declaration()
 {
   uint8_t global = parse_variable("Expect variable name.");
@@ -459,6 +561,12 @@ static void statement()
   {
     print_statement();
   }
+  else if (match(TOKEN_LEFT_BRACE))
+  {
+    begin_scope();
+    block();
+    end_scope();
+  }
   else
   {
     expression_statement();
@@ -483,6 +591,8 @@ static void declaration()
 bool compile(const char *source, Chunk *chunk)
 {
   init_scanner(source);
+  Compiler compiler;
+  init_compiler(&compiler);
   compiling_chunk = chunk;
 
   parser.had_error = false;
